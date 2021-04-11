2fd215c530cedb62baf4bbe3f599d2d558b98837
samvalvi <samuelvalerin@gmail.com>
2021-04-10 18:55:53 -0600

    final commit

--- src/app.py
+++ src/app.py
@@ -18,9 +18,34 @@ def getproduct(product_name):
 
 @app.route('/product', methods=['POST'])
 def addproduct():
-    new_product = request.json()
-    return jsonify(new_product)
+    new_product = {
+        "name":request.json['name'], 
+        "quantity":request.json['quantity'], 
+        "price":request.json['price'],
+    }
+    products.append(new_product)
+    return jsonify({"message": "New product added", "products": products})
 
 
+@app.route('/product/<string:product_name>', methods=['PUT'])
+def  updateproduct(product_name):
+    update_product = [i for i in products if i['name'] == product_name]
+    if len(update_product) > 0:
+        update_product[0]['name'] = request.json['name'],
+        update_product[0]['quantity'] = request.json['quantity'],
+        update_product[0]['price'] = request.json['price']
+        return jsonify({"message":"product updated", "products":products})
+    return jsonify({"message": "product not found"})
+    
+    
+@app.route('/product/<string:product_name>', methods=['DELETE'])
+def deleteproduct(product_name):
+    delete_product = [i for i in products if i['name'] == product_name]
+    if len(delete_product) > 0:
+        products.remove(delete_product[0])
+        return jsonify({"message": "product deleted", "products":products})
+    return jsonify({"message": "product not found"})   
+    
+    
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+    app.run(host="localhost", port=3000, debug=True)
